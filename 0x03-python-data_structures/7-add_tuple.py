185f7ae0507eaeb3b3a078bf8dbb44e3cd97a1a1
Raymond Omoraka <omorakaraymond2000@gmail.com>
2023-03-13 14:59:06 -0700

    task 7 corrected

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a, tuple_b):
-    if len(tuple_a) == 1:
-        c = tuple_a[0] + tuple_b[0], 0 + tuple_b[1]
-    elif len(tuple_b) == 1:
-        c = tuple_a[0] + tuple_b[0], tuple_a[1] + 0
-    elif len(tuple_a) == 0:
-        c = 0 + tuple_b[0], 0 + tuple_b[1]
-    elif len(tuple_b) == 0:
-        c = tuple_a[0] + 0, tuple_a[1] + 0
-    elif len(tuple_b) == 1 and len(tuple_a) == 0:
-        c = 0 + tuple_b[0], 0
-    elif len(tuple_a) == 1 and len(tuple_b) == 0:
-        c = tuple_a[0] + 0, 0
-    elif len(tuple_b) == 0 and len(tuple_a) == 0:
-        c = 0, 0
-    elif len(tuple_b) == 1 and len(tuple_a) == 1:
-        c = tuple_a[0] + tuple_b[0], 0
-    else:
-        c = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
-    return c
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    """Add two tuples."""
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 0:
+            tuple_a = 0, 0
+        else:
+            tuple_a = tuple_a[0], 0
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 0:
+            tuple_b = 0, 0
+        else:
+            tuple_b = tuple_b[0], 0
+
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
